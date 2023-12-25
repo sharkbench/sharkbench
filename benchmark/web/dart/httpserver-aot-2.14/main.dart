42d2314b7e1e5a8fa1ac2e382129e9904eb1625d
Tien Do Nam <dev.tien.donam@gmail.com>
2023-12-25 15:18:19 +0100

    optimize dart web benchmark

--- benchmark/web/dart/httpserver-aot-2.14/main.dart
+++ benchmark/web/dart/httpserver-aot-2.14/main.dart
@@ -1,18 +1,21 @@
 import 'dart:convert';
 import 'dart:io';
-import 'package:http/http.dart' as http;
 
 const port = 3000;
 
 void main() async {
   final url = Uri.http('web-data-source', '/data.json');
+  final httpClient = HttpClient();
+
   final server = await HttpServer.bind('0.0.0.0', port);
   server.listen((HttpRequest request) async {
     final path = request.uri.path;
     final params = request.uri.queryParameters;
     final symbol = params['symbol'] as String;
-    final response = await http.get(url);
-    final json = jsonDecode(response.body);
+
+    final httpClientReq = await httpClient.getUrl(url);
+    final httpClientRes = await httpClientReq.close();
+    final json = jsonDecode(await httpClientRes.transform(utf8.decoder).join());
     switch (path) {
       case '/api/v1/periodic-table/element':
         final entry = json[symbol] as Map<String, dynamic>;

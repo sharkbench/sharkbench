ff0dfe58de4f28b8400274038199b85703150372
Tien Do Nam <dev.tien.donam@gmail.com>
2023-12-25 16:19:31 +0100

    add flask web benchmark

---
+++ benchmark/web/python/flask-2.3-python-3.8/main.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, Response
+import json
+import requests
+
+app = Flask(__name__)
+app.json.sort_keys = False
+
+def fetchData(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.json()
+
+@app.route('/', methods=['GET'])
+def index():
+    return 'Hello world!'
+
+@app.route('/api/v1/periodic-table/element', methods=['GET'])
+def get_element():
+    symbol = request.args.get('symbol')
+    json_data = fetchData('http://web-data-source/data.json')
+    entry = json_data[symbol]
+    return Response(json.dumps({
+        'name': entry['name'],
+        'number': entry['number'],
+        'group': entry['group']
+    }, separators=(',', ':')), mimetype='application/json')
+
+@app.route('/api/v1/periodic-table/shells', methods=['GET'])
+def get_shells():
+    symbol = request.args.get('symbol')
+    json_data = fetchData('http://web-data-source/data.json')
+    shells = json_data[symbol]['shells']
+    return Response(json.dumps({'shells': shells}, separators=(',', ':')), mimetype='application/json')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000)

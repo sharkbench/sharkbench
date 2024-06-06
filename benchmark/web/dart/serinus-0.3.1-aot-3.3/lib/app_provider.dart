7568aceea013e98c4865fb415c1fdab8fb27647c
Francesco Vallone <vallonefrancesco587@gmail.com>
2024-06-06 23:44:08 +0200

    fix: remove cast

--- benchmark/web/dart/serinus-0.3.1-aot-3.3/lib/app_provider.dart
+++ benchmark/web/dart/serinus-0.3.1-aot-3.3/lib/app_provider.dart
@@ -15,7 +15,7 @@ class AppProvider extends Provider {
     final tmpReq = await httpClient.getUrl(elementUrl);
     final tmpRes = await tmpReq.close();
     final json = jsonDecode(await tmpRes.transform(utf8.decoder).join());
-    final entry = json[symbol] as Map<String, dynamic>;
+    final entry = Map<String, dynamic>.from(json[symbol]);
     return {
       'name': entry['name'],
       'number': entry['number'],
@@ -26,8 +26,7 @@ class AppProvider extends Provider {
   Future<Map<String, dynamic>> getShells(String symbol) async {
     final tmpReq = await httpClient.getUrl(shellsUrl);
     final tmpRes = await tmpReq.close();
-    final json = jsonDecode(await tmpRes.transform(utf8.decoder).join())
-        as Map<String, dynamic>;
+    final json = Map<String, dynamic>.from(jsonDecode(await tmpRes.transform(utf8.decoder).join()));
     return {
       'shells': json[symbol],
     };

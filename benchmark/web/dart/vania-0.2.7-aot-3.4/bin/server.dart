c99e2f36b4180b71972ef6f3284b9d4a1dd33701
Javad Zobeidi <j.zobeidi89@gmail.com>
2024-06-10 10:33:18 +0330

    Vania  Add controller route

--- benchmark/web/dart/vania-0.2.7-aot-3.4/bin/server.dart
+++ benchmark/web/dart/vania-0.2.7-aot-3.4/bin/server.dart
@@ -1,18 +1,15 @@
 import 'dart:convert';
 import 'dart:io';
-
 import 'package:vania/vania.dart';
 
 Application? app;
 
-final elementUrl = Uri.http('web-data-source', '/element.json');
-final shellsUrl = Uri.http('web-data-source', '/shells.json');
-final httpClient = HttpClient();
-
 void main() async {
   app = Application();
   await app?.initialize(config: {
-    'providers': <ServiceProvider>[RouteServiceProvder()]
+    'providers': <ServiceProvider>[
+      RouteServiceProvder(),
+    ],
   });
 }
 
@@ -22,33 +19,51 @@ class RouteServiceProvder extends ServiceProvider {
 
   @override
   Future<void> register() async {
-    Router.basePrefix('api/v1');
-    Router.get('/periodic-table/element', (Request request) async {
-      final tmpReq = await httpClient.getUrl(elementUrl);
-      final tmpRes = await tmpReq.close();
-      final json = jsonDecode(await tmpRes.transform(utf8.decoder).join())
-          as Map<String, dynamic>;
-      print(request.all());
-      String symbol = request.query('symbol');
+    Router.get('/api/v1/periodic-table/element', ApiController().element);
+    Router.get('api/v1/periodic-table/shells', ApiController().shells);
+  }
+}
 
-      final entry = json[symbol] as Map<String, dynamic>;
-      return Response.json({
-        'name': entry['name'],
-        'number': entry['number'],
-        'group': entry['group'],
-      });
+class ApiController extends Controller {
+  static ApiController? _singleton;
+
+  factory ApiController() {
+    if (_singleton == null) {
+      _singleton = ApiController._internal();
+    }
+    return _singleton!;
+  }
+
+  ApiController._internal();
+
+  final elementUrl = Uri.http('web-data-source', '/element.json');
+  final shellsUrl = Uri.http('web-data-source', '/shells.json');
+  final httpClient = HttpClient();
+
+  Future element(Request request) async {
+    final tmpReq = await httpClient.getUrl(elementUrl);
+    final tmpRes = await tmpReq.close();
+    final json = jsonDecode(await tmpRes.transform(utf8.decoder).join())
+        as Map<String, dynamic>;
+    String symbol = request.query('symbol');
+
+    final entry = json[symbol] as Map<String, dynamic>;
+    return Response.json({
+      'name': entry['name'],
+      'number': entry['number'],
+      'group': entry['group'],
     });
+  }
 
-    Router.get('/periodic-table/shells', (Request request) async {
-      final tmpReq = await httpClient.getUrl(shellsUrl);
-      final tmpRes = await tmpReq.close();
-      final json = jsonDecode(await tmpRes.transform(utf8.decoder).join())
-          as Map<String, dynamic>;
+  Future shells(Request request) async {
+    final tmpReq = await httpClient.getUrl(shellsUrl);
+    final tmpRes = await tmpReq.close();
+    final json = jsonDecode(await tmpRes.transform(utf8.decoder).join())
+        as Map<String, dynamic>;
 
-      String symbol = request.input('symbol');
-      return Response.json({
-        'shells': json[symbol],
-      });
+    String symbol = request.input('symbol');
+    return Response.json({
+      'shells': json[symbol],
     });
   }
 }

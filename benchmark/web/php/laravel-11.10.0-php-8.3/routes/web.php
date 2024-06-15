46fb9fbd6e2459129ac92f0cab2f1cf315fcc46e
Javad Zobeidi <45537050+javad-zobeidi@users.noreply.github.com>
2024-06-15 19:34:04 +0330

    Add Laravel Fixed shells (#9)

---
+++ benchmark/web/php/laravel-11.10.0-php-8.3/routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/api/v1/periodic-table/element', function () {
+    $symbol = $_GET['symbol'];
+    $response = Http::get('http://web-data-source/element.json');
+    $data = $response->json($symbol);
+    return response()->json($data);
+});
+
+
+Route::get('/api/v1/periodic-table/shells', function () {
+    $symbol = $_GET['symbol'];
+    $response = Http::get('http://web-data-source/shells.json');
+    $data = $response->json($symbol);
+    return response()->json(["shells" => $data]);
+});

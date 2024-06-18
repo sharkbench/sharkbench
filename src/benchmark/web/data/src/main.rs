605be1695e0517e524a30ae1f6eeaea9926badbf
Tien Do Nam <dev.tien.donam@gmail.com>
2024-06-18 17:58:48 +0200

    benchmark: improve web data source

--- src/benchmark/web/data/src/main.rs
+++ src/benchmark/web/data/src/main.rs
@@ -1,15 +1,38 @@
-use axum::Router;
-use axum::routing::get;
+use std::convert::Infallible;
+use std::fs;
+use http_body_util::Full;
+use hyper::{Request, Response};
+use hyper::body::Bytes;
+use hyper::server::conn::http1;
+use hyper::service::service_fn;
+use hyper_util::rt::TokioIo;
 
 #[tokio::main]
-async fn main() {
-    let element_file: &'static String = Box::leak(Box::new(std::fs::read_to_string("static/element.json").unwrap()));
-    let shells_file: &'static String = Box::leak(Box::new(std::fs::read_to_string("static/shells.json").unwrap()));
-
-    let app = Router::new()
-        .route("/element.json", get(|| async { element_file.to_owned() }))
-        .route("/shells.json", get(|| async { shells_file.to_owned() }));
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let element_file: &'static String = Box::leak(Box::new(fs::read_to_string("static/element.json").unwrap()));
+    let shells_file: &'static String = Box::leak(Box::new(fs::read_to_string("static/shells.json").unwrap()));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:80").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+
+    loop {
+        let (stream, _) = listener.accept().await?;
+        let io = TokioIo::new(stream);
+
+        tokio::task::spawn(async move {
+            if let Err(err) = http1::Builder::new()
+                .serve_connection(io, service_fn(|request: Request<hyper::body::Incoming>| async move {
+                    let path: &str = request.uri().path();
+
+                    Ok::<Response<Full<Bytes>>, Infallible>(Response::new(Full::new(Bytes::from(match path {
+                        "/element.json" => element_file.as_bytes(),
+                        "/shells.json" => shells_file.as_bytes(),
+                        _ => b"404 Not Found",
+                    }))))
+                }))
+                .await
+            {
+                eprintln!("Error serving connection: {:?}", err);
+            }
+        });
+    }
 }

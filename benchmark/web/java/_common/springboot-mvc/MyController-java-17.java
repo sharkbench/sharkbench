86dea06856760d6ff0622d0c26ce01c2163584a5
Tien Do Nam <dev.tien.donam@gmail.com>
2025-05-07 05:07:01 +0200

    benchmark: add connection pooling to Spring Boot MVC

--- benchmark/web/java/_common/springboot-mvc/MyController-java-17.java
+++ benchmark/web/java/_common/springboot-mvc/MyController-java-17.java
@@ -1,16 +1,37 @@
 package com.example.demo;
 
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import jakarta.annotation.PostConstruct;
+
 import java.util.Map;
 
 @RestController
 class MyController {
+    private RestTemplate restTemplate;
 
-    private final RestTemplate restTemplate = new RestTemplate();
+    @PostConstruct
+    public void init() {
+        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
+        connectionManager.setMaxTotal(64);
+        connectionManager.setDefaultMaxPerRoute(32);
+
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setConnectionManager(connectionManager)
+                .build();
+
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+        requestFactory.setHttpClient(httpClient);
+
+        this.restTemplate = new RestTemplate(requestFactory);
+    }
 
     @GetMapping("/api/v1/periodic-table/element")
     public Map<String, Object> getElement(@RequestParam String symbol) {

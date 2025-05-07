4d28d5da448ed70b9f1733a4b05d38a20072e897
Tien Do Nam <dev.tien.donam@gmail.com>
2025-05-07 04:05:50 +0200

    benchmark: add connection pooling to Ruby on Rails

--- benchmark/web/ruby/rails-8-ruby-3.2/app/services/element_service.rb
+++ benchmark/web/ruby/rails-8-ruby-3.2/app/services/element_service.rb
@@ -1,20 +1,30 @@
 class ElementService
   require 'net/http'
   require 'json'
+  require 'connection_pool'
+
+  @@connection_pool = ConnectionPool.new(size: 32, timeout: 5) do
+    http = Net::HTTP.new('web-data-source', 80)
+    http.keep_alive_timeout = 30
+    http.start
+    http
+  end
 
   def self.fetch_element(symbol)
-    uri = URI('http://web-data-source/element.json')
-    response = Net::HTTP.get(uri)
-    element_data = JSON.parse(response)
-
-    element_data[symbol]
+    @@connection_pool.with do |http|
+      request = Net::HTTP::Get.new('/element.json')
+      response = http.request(request)
+      element_data = JSON.parse(response.body)
+      element_data[symbol]
+    end
   end
 
   def self.fetch_shells(symbol)
-    uri = URI('http://web-data-source/shells.json')
-    response = Net::HTTP.get(uri)
-    shells_data = JSON.parse(response)
-
-    shells_data[symbol]
+    @@connection_pool.with do |http|
+      request = Net::HTTP::Get.new('/shells.json')
+      response = http.request(request)
+      shells_data = JSON.parse(response.body)
+      shells_data[symbol]
+    end
   end
 end

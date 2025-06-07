94ad3a9981e4f5b26a965cd8f1676792d198831c
Jost Alemann <58050402+ddogfoodd@users.noreply.github.com>
2025-06-07 22:22:01 +0000

    benchmark: update zig; slimify build.zig (#26)
    * benchmark: update zig
    
    * benchmark: remove zig build run cmd

--- benchmark/computation/zig/zig-0.14/build.zig
+++ benchmark/computation/zig/zig-0.14/build.zig
@@ -17,11 +17,4 @@ pub fn build(b: *std.Build) void {
     });
 
     b.installArtifact(exe);
-
-    const run_cmd = b.addRunArtifact(exe);
-
-    run_cmd.step.dependOn(b.getInstallStep());
-
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
 }

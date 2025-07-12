2c0272eed795aa39796c461b7c9d084cd0e233e7
Prashant Nigam <62440911+Prashant4900@users.noreply.github.com>
2025-07-12 20:25:53 +0530

    Update router.dart

--- benchmark/web/dart/sarus-0.0.1-aot-3.5/lib/config/router.dart
+++ benchmark/web/dart/sarus-0.0.1-aot-3.5/lib/config/router.dart
@@ -3,7 +3,6 @@ import 'package:sarus/sarus.dart';
 import '../api/endpoints.dart';
 
 final router = Router(
-  globalMiddleware: [logRequests()],
   routes: [
     Route(prefix: '/api/v1', endpoints: [PeriodicEndpoints()]),
   ],

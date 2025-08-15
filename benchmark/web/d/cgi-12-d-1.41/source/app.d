db43416a6a19704850b11ee5fa27452fbf330494
Sergei Giniatulin <kornburn@gmail.com>
2025-08-15 17:40:13 +0300

    Fix with processes
    removed cache code for client. Change the sub configuration to processes

--- benchmark/web/d/cgi-12-d-1.41/source/app.d
+++ benchmark/web/d/cgi-12-d-1.41/source/app.d
@@ -1,5 +1,5 @@
 import arsd.cgi;
-import arsd.http2 : ICache, Uri, HttpClient, HttpResponse, HttpRequestParameters;
+import arsd.http2 : get, Uri, HttpClient, HttpResponse, HttpRequestParameters;
 import arsd.jsvar;
 
 struct Element {
@@ -12,52 +12,17 @@ struct Shell {
     ubyte[] shells;
 }
 
-class myCache: ICache {
-    __gshared HttpResponse[HttpRequestParameters] aa;
-
-    const(HttpResponse)* getCachedResponse(HttpRequestParameters request) {
-        return &aa[request];
-    }
-
-    bool cacheResponse(HttpRequestParameters request, HttpResponse response) {
-        aa[request] = response;
-        return true;
-    }
-}
-
-
 class ApiV1 : WebObject {
-    static HttpClient internClient;
-    myCache internCache;
-
-    void makeClient() {
-        if (internClient is null) {
-            auto cc = new myCache();
-            internCache = cc;
-            auto c = new HttpClient(internCache);
-            c.keepAlive = true;
-            internClient = c;
-        }
-    }
-
-    this() {
-        makeClient();
-    }
-
     @DefaultFormat("json"):
     auto element(string symbol) {
-        auto request = internClient.request(Uri("http://web-data-source/element.json"));
-        request.send();
-        auto response = request.waitForCompletion();
+        auto response = get(Uri("http://web-data-source/element.json")).waitForCompletion;
         auto entry = response.contentJson()[symbol];
         return entry.get!Element;
     }
 
     @DefaultFormat("json"):
     auto shells(string symbol) {
-        auto request = internClient.request(Uri("http://web-data-source/shells.json"));
-        request.send();
-        auto response = request.waitForCompletion();
+        auto response = get(Uri("http://web-data-source/shells.json")).waitForCompletion();
         auto entry = response.contentJson()[symbol];
         return Shell(entry.get!(ubyte[]));
     }
@@ -71,6 +36,5 @@ void requestHandler(Cgi cgi) {
 
 void main() {
     auto server = RequestServer("0.0.0.0", 3000);
-    //server.numberOfThreads = 100;
     server.serve!requestHandler();
 }

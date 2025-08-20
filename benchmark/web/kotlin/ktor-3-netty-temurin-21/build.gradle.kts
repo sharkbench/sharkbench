c11a586a19561ae9fac2fb0a317cb5b48f2806a3
Rúben Caceiro <11026564+rcaceiro@users.noreply.github.com>
2025-08-20 16:18:20 +0100

    Update build.gradle.kts

--- benchmark/web/kotlin/ktor-3-netty-temurin-21/build.gradle.kts
+++ benchmark/web/kotlin/ktor-3-netty-temurin-21/build.gradle.kts
@@ -3,9 +3,9 @@ val kotlin_version: String by project
 val logback_version: String by project
 
 plugins {
-    kotlin("jvm") version "2.1.10"
-    id("io.ktor.plugin") version "3.1.2"
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.1.10"
+    kotlin("jvm") version "2.2.10"
+    id("io.ktor.plugin") version "3.2.3"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.2.10"
 }
 
 group = "com.example"

02a7f8d766278e08c8ee5846cf1540ae25cbf003
Jost Alemann <alemann@posteo.com>
2026-03-30 00:04:03 +0200

    benchmark: add zig httpz web benchmark

---
+++ benchmark/web/zig/httpz-0.15/build.zig
@@ -0,0 +1,29 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe_mod = b.createModule(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const exe = b.addExecutable(.{
+        .name = "benchmark",
+        .root_module = exe_mod,
+    });
+
+    b.installArtifact(exe);
+
+    // http.zig
+    const httpz = b.dependency("httpz", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    // the executable from your call to b.addExecutable(...)
+    exe.root_module.addImport("httpz", httpz.module("httpz"));
+}
